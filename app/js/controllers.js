8abff14e77647c40c1ef70e130f93925a7cdec4c
Alex Zaslavsky <alex.zaslavsky.1990@gmail.com>
2015-02-22 19:54:49 -0800

    feat: Added history list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,38 @@
  */
 angular.module('pomodoro')
 .controller('MainCtrl', function ($scope) {
-	$scope.startButton = 'pomodoro';
+	//STARTING VALUES
+	$scope.startButton = 'Pomodoro';
 
+
+
+	//METHODS
+	//Write out the countdown value in text form
+	$scope.formatTime = function() {
+		if ($scope.ding) {
+			return "DING!";
+		}
+		return ("0" + Math.floor($scope.time/60000)).slice(-2) + ':' + ("0" + Math.floor(($scope.time%60000)/1000)).slice(-2);
+	};
+
+	//Render the age of each history element
+	$scope.formatAge = function(timestamp) {
+		var minGone = Math.ceil((new Date().getTime() - timestamp)/60000);
+
+		//Get formatted minutes and hours
+		if (minGone < 1200) { //Less than 2 hours?  Show minutes
+			return minGone  +' minute'+ (minGone === 1 ? '' : 's') +' ago';
+		} else if (minGone < 2880) { //Less than 2 days?  Show hours
+			return Math.round(minGone/60) +' hour'+ (Math.floor(minGone/60) === 1 ? '' : 's') +' ago';
+		} else { //More than that?  Show days
+			return Math.floor(minGone/1440) +' day'+ (Math.floor(minGone/1440) === 1 ? '' : 's') +' ago';
+		}
+	};
+
+
+
+	//INFORMATION STORES
+	//Top buttons
 	$scope.buttons = [
 		{
 			name: 'Pomodoro',
@@ -22,6 +52,10 @@ angular.module('pomodoro')
 		}
 	];
 
+	//History store
+	$scope.history = [];
+
+	//Page title
 	$scope.title = 'Angular Pomodoro';
 
 	//Has the timer run out?
@@ -36,14 +70,6 @@ angular.module('pomodoro')
 	//Track whether we are in a pomodoro, long break, or short break
 	$scope.activeLoop = $scope.startButton;
 
-	//Write out the countdown value in text form
-	$scope.formatTime = function() {
-		if ($scope.ding) {
-			return "DING!";
-		}
-		return ("0" + Math.floor($scope.time/60000)).slice(-2) + ':' + ("0" + Math.floor(($scope.time%60000)/1000)).slice(-2);
-	};
-
 	//Click handler
 	$scope.click = function(e, key) {
 		var buttons = $scope.buttons;
@@ -54,26 +80,40 @@ angular.module('pomodoro')
 				$scope.loopCount++;
 
 				//Add the 900ms to prevent a "flash" at the beginning of the countdown
-				var currentTypeFullTime = $scope.time = (buttons[i].seconds * 1000) + 900;
+				var currentTypeFullTime = $scope.time = (buttons[i].seconds * 1000) - 100;
 				var loopIndex = $scope.loopCount;
 				var loopStartTime = new Date().getTime();
-				var formatted;
+				var formatted, changed;
 
 				//Update the time every quarter second
 				var timerLoop = setInterval(function(){
-					var time = currentTypeFullTime - (new Date().getTime() - loopStartTime);
+					var currentTime = new Date().getTime();
+					var time = currentTypeFullTime - (currentTime - loopStartTime);
 					var ding = false;
 
 					if ($scope.time <= 0) {
+						changed = true;
 						$scope.time = 0;
 						ding = true;
-						clearInterval(timerLoop);
 					}
 					else if ($scope.loopCount > loopIndex) {
-						clearInterval(timerLoop);
+						changed = true;
+						
 					}
 
 					time = time < 0 ? 0 : time;
+
+					if (changed === true) {
+						//Append to history
+						$scope.history.push({
+							type: buttons[i].name,
+							time: currentTime
+						});
+						$scope.activeLoop = buttons[i].key;
+
+						//Clear the interval
+						clearInterval(timerLoop);
+					}
 
 					//Render the new time
 					$scope.$apply(function(){
@@ -86,4 +126,9 @@ angular.module('pomodoro')
 			}
 		}
 	};
+
+	//History text refresh loop
+	setInterval(function(){
+		$scope.$apply();
+	}, 10000);
 });
